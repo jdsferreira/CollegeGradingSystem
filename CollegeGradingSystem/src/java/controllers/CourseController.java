1332ff33c6d10233c3454b4934f2c0c70af7fc6c
Philippe Nguyen <philippe.lam.nguyen@hotmail.com>
2022-08-24 14:05:17 -0400

    Ajouts : Servlet et JSP pour Courses

---
+++ CollegeGradingSystem/src/java/controllers/CourseController.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Course;
+
+/**
+ *
+ * @author phili
+ */
+@WebServlet(name = "CourseController", urlPatterns = {"/CourseController"})
+public class CourseController extends HttpServlet {
+
+    List<Course> listCourses = new ArrayList<>();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id1 = Integer.parseInt(request.getParameter("courseId"));
+        String name = request.getParameter("courseName");
+        int creditNumber1 = Integer.parseInt(request.getParameter("creditNumber"));
+
+        Course course1 = new Course();
+        
+        course1.setCourseId(id1);
+        course1.setCourseName(name);
+        course1.setCreditNumber(creditNumber1);
+        
+        listCourses.add(course1);
+        
+        request.setAttribute("listCourses", listCourses);
+
+        getServletContext().getRequestDispatcher("/listCourses.jsp").
+                forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
